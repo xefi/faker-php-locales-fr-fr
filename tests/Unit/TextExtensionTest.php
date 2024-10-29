cda977f584478ccbbe5773730d82ce16e3c6acab
Gautier DELEGLISE <gautier.deleglise@gmail.com>
2024-10-29 22:23:33 +0100

    :sparkles: text extension

---
+++ tests/Unit/TextExtensionTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use ReflectionClass;
+
+final class TextExtensionTest extends \Xefi\Faker\FrFr\Tests\Unit\TestCase
+{
+    protected array $paragraphs = [];
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $textExtension = new \Xefi\Faker\FrFr\Extensions\TextExtension(new \Random\Randomizer());
+        $this->paragraphs = (new ReflectionClass($textExtension))->getProperty('paragraphs')->getValue($textExtension);
+    }
+
+    public function testWords(): void
+    {
+        $sentences = array_merge(...$this->paragraphs);
+        $words = array_merge(...$sentences);
+        $result = $this->faker->words(count($words));
+
+        $result = preg_split('/\s+/', $result);
+        $words = array_map(function ($word) { return strtolower(preg_replace('/[.,]/', '', $word));}, $words);
+
+        $this->assertEqualsCanonicalizing(
+            $words,
+            $result
+        );
+    }
+}
