06e221c833935e7106cb4bea8eebcd717de915a2
Martin Soenen <martin.soenen@outlook.fr>
2025-06-04 10:25:46 +0200

    Add getLocale to Address & Company Extension

--- src/Extensions/AddressExtension.php
+++ src/Extensions/AddressExtension.php
@@ -6,6 +6,11 @@ use Xefi\Faker\Extensions\Extension;
 
 class AddressExtension extends Extension
 {
+    public function getLocale(): string|null
+    {
+        return 'fr_FR';
+    }
+
     protected $departments = [
         ['01' => 'Ain'], ['02' => 'Aisne'], ['03' => 'Allier'], ['04' => 'Alpes-de-Haute-Provence'], ['05' => 'Hautes-Alpes'],
         ['06'  => 'Alpes-Maritimes'], ['07' => 'Ardèche'], ['08' => 'Ardennes'], ['09' => 'Ariège'], ['10' => 'Aube'],
